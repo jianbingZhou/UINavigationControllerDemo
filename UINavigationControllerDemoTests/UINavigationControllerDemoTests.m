487bb3235ae1cbf7dc8811396f91bde3b2877b5a
jianbingZhou <986278051@qq.com>
2016-08-09 14:44:56 +0800

    Initial Commit

---
+++ UINavigationControllerDemoTests/UINavigationControllerDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  UINavigationControllerDemoTests.m
+//  UINavigationControllerDemoTests
+//
+//  Created by Terra MacBook on 16/8/9.
+//  Copyright © 2016年 Jianbing Zhou. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UINavigationControllerDemoTests : XCTestCase
+
+@end
+
+@implementation UINavigationControllerDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
